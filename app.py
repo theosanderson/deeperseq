f59308f6d050b4ec8ae0ddaf6b14d9cbfc9664df
theosanderson <theo@theo.io>
2023-02-20 00:43:45 +0000

    halfway

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import subprocess
+import asyncio
 import logging
+import subprocess
+import uuid
+
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -25,45 +28,69 @@ logging.basicConfig(
     ]
 )
 
+# Store running tasks and logs
+tasks = {}
+logs = {}
 
-def raw_to_bam(accession: str):
+async def raw_to_bam(accession: str, task_id: str):
     logging.info(f"Processing SRA accession {accession}")
-    yield f"Processing SRA accession {accession}"
+    logs[task_id].append(f"Processing SRA accession {accession}")
 
     # Download FASTQ files
-    cmd_fastq_dump = f'fastq-dump --split-files {accession}'
-    subprocess.run(cmd_fastq_dump, shell=True, check=True)
-
+    cmd_fastq_dump = f'fasterq-dump --split-files {accession}'
+    proc = await asyncio.create_subprocess_shell(cmd_fastq_dump)
+    await proc.wait()
     logging.info(f"Downloaded FASTQ files for {accession}")
-    yield f"Downloaded FASTQ files for {accession}"
+    logs[task_id].append(f"Downloaded FASTQ files for {accession}")
 
     # Align to reference genome using minimap2 and pipe to samtools view
     cmd_minimap2 = f'minimap2 -a ./ref.fa {accession}_1.fastq {accession}_2.fastq | samtools view -bS - > {accession}.bam'
-    subprocess.run(cmd_minimap2, shell=True, check=True)
-
+    proc = await asyncio.create_subprocess_shell(cmd_minimap2)
+    await proc.wait()
     logging.info(f"Aligned reads for {accession}")
-    yield f"Aligned reads for {accession}"
-
+    logs[task_id].append(f"Aligned reads for {accession}")
 
     cmd_sort_bam = f'samtools sort -o {accession}.sorted.bam {accession}.bam'
-    subprocess.run(cmd_sort_bam, shell=True, check=True)
-
+    proc = await asyncio.create_subprocess_shell(cmd_minimap2)
+    await proc.wait()
     logging.info(f"Sorted BAM file for {accession}")
-    yield f"Sorted BAM file for {accession}"
+    logs[task_id].append(f"Sorted BAM file for {accession}")
 
     cmd_index_bam = f'samtools index {accession}.sorted.bam'
-    subprocess.run(cmd_index_bam, shell=True, check=True)
-
+    proc = await asyncio.create_subprocess_shell(cmd_minimap2)
+    await proc.wait()
     logging.info(f"Indexed BAM file for {accession}")
-    yield f"Indexed BAM file for {accession}"
+    logs[task_id].append(f"Indexed BAM file for {accession}")
 
-    return f"Finished processing {accession}"
+    logs[task_id].append(f"Finished processing {accession}")
 
-@app.get("/align/{accession}")
+async def start_task(accession: str):
+    task_id = str(uuid.uuid4())
+    logs[task_id] = []
+    task = asyncio.create_task(raw_to_bam(accession, task_id))
+    tasks[task_id] = task
+    return task_id
+
+@app.post("/align/{accession}")
 async def align(accession: str):
     logging.info(f"Aligning {accession} to reference genome")
-    return StreamingResponse(raw_to_bam(accession))
+    task_id = await start_task(accession)
+    return {"task_id": task_id}
+
+@app.get("/poll/{task_id}")
+async def poll(task_id: str):
+    if task_id not in tasks:
+        return {"error": "Invalid task ID"}
+
+    if not tasks[task_id].done():
+        return {"status": "processing", "log": logs[task_id]}
+
+    if task_id in logs:
+        return {"status": "complete", "log": logs[task_id]}
+    else:
+        return {"status": "complete"}
     
+
 
 @app.get("/dl_align/{accession}")
 async def index(accession: str):
---
+++ js/src/App.js
@@ -0,0 +1,139 @@
+import React, { useState,useRef , useEffect} from 'react';
+import axios from 'axios';
+import igv from "https://cdn.jsdelivr.net/npm/igv@2.13.11/dist/igv.esm.min.js"
+const backend = "http://localhost:80"
+
+function Alignment() {
+  const [accession, setAccession] = useState('');
+  const [taskID, setTaskID] = useState('');
+  const [logs, setLogs] = useState([]);
+  const [status, setStatus] = useState('');
+  const [error, setError] = useState('');
+  const [bamURL, setBamURL] = useState('');
+  const [baiURL, setBaiURL] = useState('');
+  const igvDiv = useRef(null);
+  useEffect(() => {
+    if (bamURL && baiURL) {
+      igv.createBrowser(igvDiv.current, {
+        //ref is /ref.fa
+        reference: {
+          id: 'NC_045512.2',
+          fastaURL: '/ref.fa',
+        },
+        locus: 'NC_045512.2:1-29903',
+        tracks: [
+          {
+            type: 'alignment',
+            format: 'bam',
+            url: bamURL,
+            indexURL: baiURL,
+            name: 'Aligned Reads',
+          },
+        ],
+      });
+    }
+  }, [bamURL, baiURL]);
+
+
+
+
+
+
+
+  const handleAccessionChange = (event) => {
+    setAccession(event.target.value);
+  };
+
+  const handleAlignmentSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`${backend}/align/${accession}`);
+      const taskID = response.data.task_id;
+      setTaskID(taskID);
+      setStatus('processing');
+      setError('');
+      setLogs([]);
+      pollTask(taskID);
+    } catch (error) {
+      console.error(error);
+      setError('Error starting alignment task');
+    }
+  };
+
+  const pollTask = async (taskID) => {
+    try {
+      const response = await axios.get(`${backend}/poll/${taskID}`);
+      const taskStatus = response.data.status;
+      if (taskStatus === 'processing') {
+        setLogs(response.data.log);
+        setTimeout(() => pollTask(taskID), 5000);
+      } else if (taskStatus === 'complete') {
+        setLogs(response.data.log);
+        setStatus('complete');
+        setBamURL(`${backend}/dl_align/${accession}`);
+        setBaiURL(`${backend}/index/${accession}`);
+      }
+    } catch (error) {
+      console.error(error);
+      setError('Error polling alignment task');
+      setTimeout(() => pollTask(taskID), 5000);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Alignment</h1>
+      <form onSubmit={handleAlignmentSubmit}>
+        <label>
+          SRA Accession ID:
+          <input type="text" value={accession} onChange={handleAccessionChange} />
+        </label>
+        <button type="submit">Start Alignment</button>
+      </form>
+      {error && <p>{error}</p>}
+      {status === 'processing' && 
+
+        <div>
+          <h2>Alignment Processing</h2>
+          <h3>Logs</h3>
+          <ul>
+            {logs.map(
+              (log, index) =>
+                log && (
+                  <li key={index}>
+                    <pre>{log}</pre>
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      }
+
+      {status === 'complete' && (
+        <div>
+          <h2>Alignment Complete</h2>
+          <h3>Aligned Reads</h3>
+          <div id="igv-div" style={{ width: '100%', height: '1000px' }} ref={igvDiv} />
+          
+          
+          
+          <h3>Logs</h3>
+          <ul>
+            {logs.map(
+              (log, index) =>
+                log && (
+                  <li key={index}>
+                    <pre>{log}</pre>
+                  </li>
+                )
+            )}
+
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+export default Alignment;
